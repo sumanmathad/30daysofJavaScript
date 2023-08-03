bdcefe4a88a6042da2de3f0b4350677b10958abe
suman_mathad <suman.mathad@gmail.com>
2023-08-03 17:42:20 +0530

    day1 set up for js

---
+++ day1/excercises/variables.js
@@ -0,0 +1,46 @@
+/*
+Write a single line comment which says, comments can make code readable
+Write another single comment which says, Welcome to 30DaysOfJavaScript
+Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. 
+Check the data type of each variable
+Declare four variables without assigning values
+Declare four variables with assigned values
+Declare variables to store your first name, last name, marital status, country and age in multiple lines
+Declare variables to store your first name, last name, marital status, country and age in a single line
+Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+*/
+
+//*********Answers************
+
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable, 
+easy to reuse and informative
+*/
+
+let alpha = "a";
+let line = "this is a line";
+let num = 0;
+let isWorking = true;
+let nothing = null;
+let notinitialised;
+let empty ='';
+
+console.log(alpha,line, num, nothing, notinitialised, empty, isWorking);
+console.log(typeof nothing, typeof empty, typeof notinitialised, typeof alpha, typeof line, typeof num);
+
+const fname= "suman";
+const lname = "mathad";
+const age = 31;
+const country="india";
+
+const firstname="suman", lastname="mathad", ageValue=31, Nationality="India";
+
+
+
+
